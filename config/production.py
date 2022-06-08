1a22f7a25ffc5b7adbe0f54af43debd622ce3e43
jeong <gitwalker@naver.com>
2022-06-09 01:05:20 +0900

    log 기능 삽입

--- config/production.py
+++ config/production.py
@@ -1,3 +1,4 @@
+from logging.config import dictConfig
 
 
 from config.default import *
@@ -5,3 +6,26 @@ from config.default import *
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'f\xe6cE=\xb6-\xd3\xd8\xc1\xdb?\xde\xae\x80\xd4'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024*1024 * 5, #5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
